904ea668c5d25c589ab20b959e9e576c0a0c2116
showel22 <schowel22@gmail.com>
2018-08-22 18:57:22 -0400

    Add Database Doc and basic chat

---
+++ assets/documentation/sampleObject.js
@@ -0,0 +1,35 @@
+[
+    {
+        driver: 'John Smith',
+        numberOfPassengers: 3,
+        approvedPassengers: [
+            "Jane Doe"
+        ],
+        startDateTime: 9023478283743242,
+        startLocation: 'Buckhead',
+        passengers: [
+            {
+                name: "Jane Doe",
+                approved: false,
+            },
+            {},
+            {}
+        ],
+        chat: [
+            {
+                text: "Can I ride?",
+                name: "John Doe",
+                timestamp: 23470238470293,
+                type: "Approval"
+            },
+            {
+                text: "Hey, How's it going?",
+                name: "John Doe",
+                timestamp: 23470238470293,
+                type: "chat"
+            }
+        ]
+    },
+    {},
+    {}
+];
