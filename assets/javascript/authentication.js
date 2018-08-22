9abe2664707c5a4596afc6db553f0f8eead6d820
showel22 <schowel22@gmail.com>
2018-08-22 19:20:43 -0400

    Adding initial Auth

---
+++ assets/javascript/authentication.js
@@ -0,0 +1,43 @@
+$(document).ready(() => {
+
+    var authentication = firebase.auth();
+    var googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    if (!!USER_TOKEN) {
+        $('#auth').text('Sign Out');
+    } else {
+        $('#auth').text('Sign In');
+    }
+
+    $('#auth').click(function () {
+        if (!!USER_TOKEN) {
+            $('#auth').text('Sign Out');
+            firebase.auth().signOut().then(function () {
+                // Sign-out successful.
+                console.log("Signed Out");
+            }).catch(function (error) {
+                // An error happened.
+                console.log("Error on Sign Out");
+            });
+        } else {
+            $('#auth').text('Sign In');
+            authentication.signInWithPopup(googleProvider).then(function (result) {
+                if (result.credential) {
+                    USER_TOKEN = result.credential.accessToken;
+                }
+                // Set the User
+                USER = result.user;
+
+            }).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                var email = error.email;
+
+                var credential = error.credential;
+                console.log(errorMessage);
+            });
+        }
+    });
+
+}); 
---
+++ assets/javascript/globals.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyAX0li5itgvRU6kvUHLleDWH-r1Fb0M12E",
+    authDomain: "carpoolapp-14ee6.firebaseapp.com",
+    databaseURL: "https://carpoolapp-14ee6.firebaseio.com",
+    projectId: "carpoolapp-14ee6",
+    storageBucket: "carpoolapp-14ee6.appspot.com",
+    messagingSenderId: "957003689395"
+};
+firebase.initializeApp(config);
+
+// Global variable to hold current user
+var USER;
+var USER_TOKEN;
