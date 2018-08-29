8c1a766599237d7b4890c08a532dc087eb40566e
showel22 <schowel22@gmail.com>
2018-08-29 19:51:45 -0400

    Order Schedule

--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -39,7 +39,7 @@ var CURRENT_PAGE = {
 
 
 // Test Data Comment this out before committing
-
+/*
 USER = {
     name: 'Steven Howell',
     email: 'schyoyo@gmail.com',
@@ -47,9 +47,9 @@ USER = {
     emailVerified: true,
     uid: 'syGrikJ6w5OzYFLWdI66Tz5h4Kh1'
 };
-
+*/
 
 $(document).ready(function () {
     CURRENT_PAGE.login();
-   CURRENT_PAGE.schedule();
+  // CURRENT_PAGE.schedule();
 });
